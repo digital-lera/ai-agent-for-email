0e1912b65319350dadebbb58d80bb72d175d026c
digital-lera <valeria.strygina.work@gmail.com>
2026-02-18 11:41:36 +0300

    Restructured scripts

--- scripts/pdf_parse.py
+++ scripts/pdf_parse.py
@@ -1,6 +1,6 @@
 import pdfplumber
 
-with pdfplumber.open("files/file.pdf") as pdf, open("email.txt", "w", encoding="utf-8") as f:
+with pdfplumber.open("input_data/file.pdf") as pdf, open("input_data/email.txt", "w", encoding="utf-8") as f:
 
     for page in pdf.pages:
         t = page.extract_text()
