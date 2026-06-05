cd93c6ce822ab81799e1e2be2a35f44d750dcb5a
digital-lera <valeria.strygina.work@gmail.com>
2026-06-05 11:28:34 +0300

    edited paths for images

--- src/scripts/pdf_parse.py
+++ src/scripts/pdf_parse.py
@@ -17,7 +17,7 @@ def pdf_parse():
 
     print(f"Вложение {filename} прочитано")
 
-    images = convert_from_path(f"input_data/{filename}")
+    images = convert_from_path(input_data_dir /filename )
 
     if not(images):
         print("Изображение вложения не найдено.")
@@ -25,7 +25,7 @@ def pdf_parse():
     try: 
 
         for index, image in enumerate(images):
-            image_path = "input_data/file.png"
+            image_path = input_data_dir / "file.png"
             image.save(image_path, 'PNG')
             
         ocr = PaddleOCR(
@@ -53,6 +53,6 @@ def pdf_parse():
     except Exception as e:
         print(f"Текст не распознан, {e}")
 
-    with open("input_data/email.txt", "w") as file:
+    with open(input_data_dir / "email.txt", "w") as file:
         file.write('\n'.join(rec_texts))
 
