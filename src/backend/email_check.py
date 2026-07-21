f20ff3c90d3f818d44421afebd26495771511ba2
digital-lera <valeria.strygina.work@gmail.com>
2026-07-21 09:53:43 +0300

    forgor print

--- src/backend/email_check.py
+++ src/backend/email_check.py
@@ -93,7 +93,7 @@ def check_email(socketio) -> int:
         message_ids = data[0].split() if data and data[0] else []
         print(f"Найдено непрочитанных сообщений: {len(message_ids)}", flush=True)
 
-        imap.list()
+        print(imap.list())
 
         for message_number in message_ids:
             try:
